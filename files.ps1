44eb40c4e72df134800a735366f6b7e93d36aa23
mlunadevops <mluna@ste.com.ve>
2025-05-11 08:46:22 -0400

    Inicializando estructura de DevOps scripts

---
+++ files.ps1
@@ -0,0 +1,4 @@
+New-Item -Path "docker/.gitkeep" -ItemType File
+New-Item -Path "kubernetes/.gitkeep" -ItemType File
+New-Item -Path "ansible/.gitkeep" -ItemType File
+New-Item -Path "terraform/.gitkeep" -ItemType File
